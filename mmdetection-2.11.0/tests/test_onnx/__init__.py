049522d6f289d6af7cd46b5b40201d4663b1248a
HaoTian Hu <54016917+hht1996ok@users.noreply.github.com>
2023-08-16 09:30:08 +0800

    Delete mmdetection-2.11.0 directory

--- mmdetection-2.11.0/tests/test_onnx/__init__.py
+++
@@ -1,6 +0,0 @@
-from .utils import (WrapFunction, convert_result_list, ort_validate,
-                    verify_model)
-
-__all__ = [
-    'WrapFunction', 'verify_model', 'convert_result_list', 'ort_validate'
-]
